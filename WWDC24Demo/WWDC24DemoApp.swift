1be60548a51e2d7481f0da28d4da40e10330178f
Terry Kuo <terrykuo501@gmail.com>
2024-06-13 08:53:45 +0800

    Initial Commit

---
+++ WWDC24Demo/WWDC24DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WWDC24DemoApp.swift
+//  WWDC24Demo
+//
+//  Created by Terry Kuo on 2024/6/13.
+//
+
+import SwiftUI
+
+@main
+struct WWDC24DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WWDC24DemoTests/WWDC24DemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  WWDC24DemoTests.swift
+//  WWDC24DemoTests
+//
+//  Created by Terry Kuo on 2024/6/13.
+//
+
+import Testing
+@testable import WWDC24Demo
+
+struct WWDC24DemoTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
